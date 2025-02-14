6c9614e706d2387909c8302c1f512675f54a939e
filonovets <dima.aristow.2001@gmail.com>
2025-02-14 20:34:49 +0300

    AddCORS

--- tic-tac-toe9x9/tic-tac-toe9x9_back/auth-service/internal/api/api.go
+++ tic-tac-toe9x9/tic-tac-toe9x9_back/auth-service/internal/api/api.go
@@ -2,8 +2,10 @@ package api
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 type Server struct {
@@ -18,6 +20,12 @@ func NewServer(ip string, port int, uc Usecase) *Server {
 		Address: fmt.Sprintf("%s:%d", ip, port),
 		uc:      uc,
 	}
+	srv.Server.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins:     []string{"http://127.0.0.1:3000"},
+		AllowMethods:     []string{http.MethodGet, http.MethodHead, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete},
+		AllowHeaders:     []string{"Content-Type", "Authorization", "Cookie"},
+		AllowCredentials: true,
+	}))
 	srv.Server.POST("/singIn", srv.SingInHandler)
 	srv.Server.POST("/singUp", srv.SingUpHandler)
 	srv.Server.GET("/stayAuth", srv.StayAuth)
--- tic-tac-toe9x9/tic-tac-toe9x9_back/go.mod
+++ tic-tac-toe9x9/tic-tac-toe9x9_back/go.mod
@@ -4,7 +4,7 @@ go 1.22.2
 
 require (
 	github.com/golang-jwt/jwt/v5 v5.2.1
-	github.com/labstack/echo/v4 v4.13.2
+	github.com/labstack/echo/v4 v4.13.3
 	github.com/lib/pq v1.10.9
 	gopkg.in/yaml.v3 v3.0.1
 )
@@ -16,7 +16,8 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.31.0 // indirect
-	golang.org/x/net v0.32.0 // indirect
+	golang.org/x/net v0.33.0 // indirect
 	golang.org/x/sys v0.28.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
+	golang.org/x/time v0.8.0 // indirect
 )
